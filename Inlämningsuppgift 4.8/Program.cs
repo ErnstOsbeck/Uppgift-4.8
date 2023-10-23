9931b2206a91a8a4769369ffecea2e837c6e13f6
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-10-23 19:37:23 +0200

    Add project files.

---
+++ Inlämningsuppgift 4.8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Net.WebRequestMethods;
+
+namespace uppgift_4._8
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en sträng bokstäver");
+            string lång = Console.ReadLine();
+            Console.WriteLine("Skriv in en sträng med exact två bokstäver");
+            string kort = Console.ReadLine();
+
+            string matchade = "";
+
+            for (int i = 0; i < kort.Length; i++)
+            {
+                char matcha = kort[i];
+
+                for (int j = 0; j < lång.Length; j++)
+                {
+                    char leta = lång[j];
+                    if(matcha == leta)
+                    {
+                        if (matcha == leta)
+                        {
+                            matchade = matchade + matcha;
+                            break;
+                        }
+                    }
+                }
+                if (matchade == kort)
+                {
+                    Console.WriteLine("Bokstäverna matchar");
+                }
+                else
+                {
+                    Console.WriteLine("Alla bokstäver matchar inte");
+                }
+            }   
+        }
+    }
+}
